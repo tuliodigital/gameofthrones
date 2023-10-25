a5567a1f9eb4d897f027e1070a441ffd0b66ede0
Emanuele Freitas <manu.freitastip@gmail.com>
2023-10-25 16:23:15 -0300

    Alteração API mapa e header

--- map.js
+++ map.js
@@ -1,26 +1,29 @@
-    function fetchQuotes() {
-        const apiUrl = "https://api.gameofthronesquotes.xyz/v1/random/5";
-
-        fetch(apiUrl)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Erro na solicitação da API");
-                }
-                return response.json();
-            })
-            .then(data => {
-                const quoteList = data.map(quote => quote.sentence);
-                const quoteListElement = document.getElementById("quote-list");
-                quoteListElement.innerHTML = ""; // Limpe o conteúdo anterior, se houver
-                quoteList.forEach(quoteText => {
-                    const li = document.createElement("li");
-                    li.textContent = quoteText;
-                    quoteListElement.appendChild(li);
-                });
-            })
-            .catch(error => {
-                console.error("Ocorreu um erro ao buscar as citações:", error);
-            });
-    }
-
-    window.addEventListener("load", fetchQuotes);
+function fetchQuotes() {
+    const apiUrl = "https://api.gameofthronesquotes.xyz/v1/random/5";
+  
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Erro na solicitação da API");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const quoteListElement = document.getElementById("quote-list");
+        quoteListElement.innerHTML = ""; // Limpe o conteúdo anterior, se houver
+        data.forEach(quote => {
+          const character = quote.character.name; // Acesse o nome do personagem corretamente
+          const quoteText = quote.sentence;
+  
+          const listItem = document.createElement("li");
+          listItem.innerHTML = `<strong>${character}:</strong> ${quoteText}`; // Use <strong> para deixar o nome em negrito
+          quoteListElement.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error("Ocorreu um erro ao buscar as citações:", error);
+      });
+  }
+  
+  window.addEventListener("load", fetchQuotes);
+  
